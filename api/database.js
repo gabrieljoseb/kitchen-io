63cbbcb34ac3329d542a9c97714dd8776908af0e
= <=>
2023-11-07 22:19:57 -0300

    added requireSsl in database.js

--- api/database.js
+++ api/database.js
@@ -3,7 +3,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })
 
 pool.on('error', (err, client) => {
